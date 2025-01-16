c07d8399502265a326979cc104e43a141e84a764
Balamurugan M <129513892+balawpugazh11@users.noreply.github.com>
2025-01-16 19:10:34 +0530

    StopAtFive.java

--- StopAtFive.java
+++ StopAtFive.java
@@ -3,11 +3,11 @@ package MentorHints;
 public class StopAtFive {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            if (i == 5) { // Check if the number is 5
+            if (i == 5) { 
                 System.out.println("Encountered 5, stopping the loop.");
-                break; // Exit the loop
+                break; 
             }
-            System.out.println(i); // Print the current number
+            System.out.println(i); 
         }
     }
 }
