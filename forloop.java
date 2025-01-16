75e593f2c4b0762f2d5932191f709fc6bcbeea39
Balamurugan M <129513892+balawpugazh11@users.noreply.github.com>
2025-01-16 10:51:45 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,10 @@
+package MentorHints;
+
+public class forloop {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 10; i++) {
+            System.out.println("The value of i is " + i);
+        }
+    }
+}
